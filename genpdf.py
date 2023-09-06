63b44fab8da1edf2439e82a3778e3aa80a3f9a5b
Rick Ho <laekov.h@gmail.com>
2023-09-06 12:12:22 +0800

    init commit

---
+++ genpdf.py
@@ -0,0 +1,29 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+
+def gen(txt):
+    matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+    fig, ax = plt.subplots()
+    fig.set_dpi(60)
+    fig.set_size_inches(16, 9)
+    for i in range(5):
+        for j in range(4):
+            ax.text(i * 240, j * 200, txt,
+                    color='gray',
+                    alpha=.2,
+                    size=14,
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    rotation=(-i * 3 + j * 4) * 10)
+    ax.set_xlim(0, 960)
+    ax.set_ylim(0, 540)
+    plt.axis('off')
+    plt.savefig('watermark.pdf', transparent=True)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        gen('sample watermark')
+    else:
+        gen(sys.argv[1])
---
+++ mark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $# != 3 ]
+then
+	echo "Usage: ./mark.sh <input filename> <output filename> <text>"
+	exit 1
+fi
+
+python3 genpdf.py $3
+pdftk $1 stamp watermark.pdf output $2
